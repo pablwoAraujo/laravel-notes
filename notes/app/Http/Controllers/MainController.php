8476030b68c3bd299d2745c766471a4f0b9f0234
pablwoAraujo <pablwo.araujo@ccc.ufcg.edu.br>
2024-12-18 23:56:47 -0300

    feat: implement basic authentication middleware

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
 
+    public function index(){
+        echo "I'm side the app!";
+    }
+
+    public function newNote(){
+        echo "I'm creating a new rote!";
+    }
 }
