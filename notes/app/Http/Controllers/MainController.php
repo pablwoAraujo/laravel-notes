598cc8f0afd1fb780ecdd79babb810b76ebda715
pablwoAraujo <pablwo.araujo@ccc.ufcg.edu.br>
2024-12-22 21:22:19 -0300

    feat: adding encryption to routes

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -3,19 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
         $id = session('user.id');
         $notes = User::find($id)->notes()->get()->toArray();
 
         return view('home', ['notes' => $notes]);
     }
 
-    public function newNote(){
+    public function newNote()
+    {
         echo "I'm creating a new rote!";
+    }
+
+    public function editNote($id)
+    {
+        $id = Operations::decryptId($id);
+        echo "I'm edditng note with id = $id";
+    }
+
+    public function deleteNote($id)
+    {
+        $id = Operations::decryptId($id);
+        echo "I'm deleting note with id = $id";
     }
 }
--- notes/routes/web.php
+++ notes/routes/web.php
@@ -15,5 +15,13 @@ Route::middleware([CheckIsNotLogged::class])->group(function () {
 Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');;
+
+    // edit note
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');;
+
+    // delete note
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');;
+
+    // logout
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');;
 });
